55213c709f4c7a424dbda7f3761bc62653350bf4
Joshua <30722988+Mystic-Manatee@users.noreply.github.com>
2018-06-05 12:15:40 +1000

    Moved things into methods

--- src/Search.java
+++ src/Search.java
@@ -1,16 +1,38 @@
 import java.util.concurrent.ThreadLocalRandom;
 public class Search {
-	public static void main(String[] args) {
-		int randomNumber;
+	public static void main(String[] args) {{
 		int[] arrayNormal = new int[1000000]; //array to store numbers as they are generated
-		{
-			for(int i=0; i<=999999; i++){
-				arrayNormal[i] += ThreadLocalRandom.current().nextInt(0, 1000001);
-			}
-		System.out.print("Array generation complete");
-		printout(arrayNormal);
+		int[] arraySorted = new int[1000000]; //array to store the numbers in ascending order
+		generateArray(arrayNormal);
+		sortArray(arraySorted, arrayNormal);
+		//printout(arrayNormal);
+		printout(arraySorted);
 		}
 	}
+	
+	public static int[] generateArray(int[] input) {
+		for(int i=0; i<=999999; i++){
+			input[i] += ThreadLocalRandom.current().nextInt(0, 1000001);
+		}
+		return input;
+	}
+	
+	public static int[] sortArray(int[] sortedArray, int[] normalArray) {
+		int temp;
+		for (int i = 0; i < normalArray.length; i++) {
+			if (i == 0 || sortedArray[i-1] <= normalArray[i]){
+				sortedArray[i] += normalArray[i];
+			}
+			else {
+				temp = sortedArray[i-1];
+				sortedArray[i-1] = normalArray[i];
+				sortedArray[i] += temp;
+				temp = 0;
+			}
+		}
+		return sortedArray;
+	}
+	
 	public static void printout(int[] input) {
 		for (int i = 0; i<input.length; i++) {
 			System.out.println(input[i]);
@@ -18,8 +40,3 @@ public class Search {
 		System.out.println("there are " + input.length + " numbers in the array");
 	}
 }
-
-/*
-
-int[] arraySorted = new int[1000000]; //array to store the numbers in ascending order
-*/
